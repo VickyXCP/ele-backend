f9678bbc508979edc4282bb16e9e5cb8bf8b199d
VickyXCP <xucuiping@fengchaotech.cn>
2018-09-26 18:10:47 +0800

    first commit

---
+++ src/config/mUtils.js
@@ -0,0 +1,46 @@
+/*
+ *存储localStorage
+ */
+export const setStore = (name, content)=>{
+	if (!name) return
+	if (typeof content !== 'string') {
+		content = JSON.stringify(content)
+	}
+	window.localStorage.setItem(name, content)
+}
+
+/*
+ *获取localStorage
+ */
+
+export const getStore = name => {
+	if (!name) return
+	return window.localStorage.getItem(name)
+}
+
+/*
+ *删除localStorage
+ */
+export const removeStore = name =>{
+	if (!name) return
+	window.localStorage.removeItem(name)
+}
+
+/*
+ *获取style样式
+ */
+
+export const getStyle = (element, attr, NumberMode = 'int') => {
+	let target;
+	//  scrollTop 获取方式不同，他不属于style， 只能通过document.body才能用
+	if (attr === 'scrollTop') {
+		target = element.scrollTop
+	} else if (element.currentStyle){
+		target = element.currentStyle[attr]
+	} else {
+	  target = document.defaultView.getComputedStyle(element, null)[attr]
+	}
+	//  在获取opacity时需要获取小数parseFloat
+	return NumberMode == 'float'?parseFloat(target):parseInt(target)
+}
+
